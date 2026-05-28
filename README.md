f4d766bed8ec53e7980b2c264121d13c62e90046
Alex Eum <alexhyuneum@gmail.com>
2026-05-28 16:10:14 -0400

    Install instructions

---
+++ README.md
@@ -0,0 +1,11 @@
+# Alex's dotfiles
+
+For a Linux Ubuntu machine
+
+1. Clone the repo
+
+`git clone --recurse-submodules https://github.com/you/dotfiles ~/.dotfiles`
+
+2. Install dependencies 
+`cd ~/.dotfiles
+./install.sh`
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# symlink configs
+ln -sf ~/.dotfiles/i3/config ~/.config/i3/config
+
+# submodules (zsh plugins)
+git submodule update --init --recursive
+
+# nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+rm nvim-linux-x86_64.tar.gz
+
+# zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+
+# eza
+sudo apt install -y eza
