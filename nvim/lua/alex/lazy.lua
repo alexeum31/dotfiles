0b52d1283496e3bdef6685f63706fce83a76d1e4
Alex Eum <alexhyuneum@gmail.com>
2026-05-28 17:20:31 -0400

    Nvim cpp autocomplete

--- nvim/lua/alex/lazy.lua
+++ nvim/lua/alex/lazy.lua
@@ -94,6 +94,32 @@ require("lazy").setup({
         })
     end,
 },
+{
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
 })
 
 -- keybinds for telescope
