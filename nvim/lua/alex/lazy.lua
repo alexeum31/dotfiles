bf26e2d9d8e203829d7ccd509a631f4dafe1036b
Alex Eum <alexhyuneum@gmail.com>
2026-05-28 17:15:22 -0400

    Telescope bug fix + keybinds

--- nvim/lua/alex/lazy.lua
+++ nvim/lua/alex/lazy.lua
@@ -69,9 +69,19 @@ require("lazy").setup({
   },
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
+    branch = "master",
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          previewer = false,
+          preview = {
+            treesitter = false,
+          }
+        }
+      })
+    end,
+},
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
@@ -85,4 +95,11 @@ require("lazy").setup({
     end,
 },
 })
+
+-- keybinds for telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files)
+vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+vim.keymap.set("n", "<leader>fb", builtin.buffers)
+vim.keymap.set("n", "<leader>fo", builtin.oldfiles)
 
